bd6907b9c2f6e80109c5cbf6c829566ce330e4eb
Odd Stråbø <oddstr13@openshell.no>
2020-12-15 00:43:37 +0100

    Update GUID

--- Jellyfin.Plugin.AniSearch/Constants.cs
+++ Jellyfin.Plugin.AniSearch/Constants.cs
@@ -3,6 +3,6 @@ namespace Jellyfin.Plugin.AniSearch
     public static class Constants
     {
         public const string PluginName = "Anime";
-        public const string PluginGuid = "a4df60c5-6ab4-412a-8f79-2cab93fb2bc5";
+        public const string PluginGuid = "776d9158-b91f-49d9-82c5-dacce63d63b8";
     }
 }
