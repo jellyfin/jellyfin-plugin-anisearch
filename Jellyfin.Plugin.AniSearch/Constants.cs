e285a1169ce418aeaec8b15d2e7991670835143f
Odd Stråbø <oddstr13@openshell.no>
2020-12-15 00:46:43 +0100

    Update plugin name

--- Jellyfin.Plugin.AniSearch/Constants.cs
+++ Jellyfin.Plugin.AniSearch/Constants.cs
@@ -2,7 +2,7 @@ namespace Jellyfin.Plugin.AniSearch
 {
     public static class Constants
     {
-        public const string PluginName = "Anime";
+        public const string PluginName = "AniSearch";
         public const string PluginGuid = "776d9158-b91f-49d9-82c5-dacce63d63b8";
     }
 }
